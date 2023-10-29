627af81b2e5308cad0bf7ecd920f0aa9c3388623
YJB <yjb@tutanota.com>
2023-10-28 21:56:50 -0400

    Automated script run and Git force push

--- update.py
+++ update.py
@@ -97,10 +97,9 @@ html_content = f"""
     <p id="more-info"><a href="{json_url}">Logs</a></p>
     
     <div>
-        <a id="donate-button" href="/donate">Donate To Save Gaza</a>
+        <a id="donate-button" href="https://yjb-gay.github.io/gaza-internet-watch/donate/">Donate To Save Gaza</a>
         <a id="image-credit" href="https://www.instagram.com/alijadallah66">Image credit Ali Jadallah - علي جادالله</a>
     </div>
-    <iframe src="https://free.timeanddate.com/clock/i936n020/n676/fn13/tt0/tw0/tm1/ts1/tb4" frameborder="0" width="88" height="36" style="position: absolute; top: 20px; right: 20px;"></iframe>
     <script>
         checkStatus();
     </script>
